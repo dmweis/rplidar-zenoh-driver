af7f92b91b910daf7219e8204a67a360fa97176d
David Weis <dweis7@gmail.com>
2023-07-16 21:22:35 +0100

    Add smart home stuff

--- src/bin/foxglove_server.rs
+++ src/bin/foxglove_server.rs
@@ -89,6 +89,86 @@ async fn main() -> anyhow::Result<()> {
     )
     .await?;
 
+    start_json_subscriber(
+        "zigbee2mqtt/ikea_dimmer",
+        zenoh_session.clone(),
+        &server,
+        "IkeaDimmer",
+        IKEA_DIMMER_JSON_SCHEMA,
+        false,
+    )
+    .await?;
+
+    start_json_subscriber(
+        "zigbee2mqtt/motion/one",
+        zenoh_session.clone(),
+        &server,
+        "MotionSensor",
+        MOTION_SENSOR_JSON_SCHEMA,
+        true,
+    )
+    .await?;
+
+    start_json_subscriber(
+        "zigbee2mqtt/motion/two",
+        zenoh_session.clone(),
+        &server,
+        "MotionSensor",
+        MOTION_SENSOR_JSON_SCHEMA,
+        true,
+    )
+    .await?;
+
+    start_json_subscriber(
+        "zigbee2mqtt/motion/three",
+        zenoh_session.clone(),
+        &server,
+        "MotionSensor",
+        MOTION_SENSOR_JSON_SCHEMA,
+        true,
+    )
+    .await?;
+
+    start_json_subscriber(
+        "zigbee2mqtt/contact/fridge",
+        zenoh_session.clone(),
+        &server,
+        "ContactSensor",
+        CONTACT_SENSOR_JSON_SCHEMA,
+        true,
+    )
+    .await?;
+
+    start_json_subscriber(
+        "zigbee2mqtt/contact/main_door",
+        zenoh_session.clone(),
+        &server,
+        "ContactSensor",
+        CONTACT_SENSOR_JSON_SCHEMA,
+        true,
+    )
+    .await?;
+
+    start_json_subscriber(
+        "zigbee2mqtt/contact/lock",
+        zenoh_session.clone(),
+        &server,
+        "ContactSensor",
+        CONTACT_SENSOR_JSON_SCHEMA,
+        true,
+    )
+    .await?;
+
+    start_json_subscriber(
+        "zigbee2mqtt/climate_sensor/one",
+        zenoh_session.clone(),
+        &server,
+        "ClimateSensor",
+        CLIMATE_SENSOR_JSON_SCHEMA,
+        true,
+    )
+    .await?;
+
     signal::ctrl_c().await.unwrap();
     info!("ctrl-c received, exiting");
 
@@ -149,3 +229,188 @@ async fn create_publisher_for_protobuf(
         )
         .await
 }
+
+const JSON_ENCODING: &str = "json";
+
+async fn start_json_subscriber(
+    topic: &str,
+    zenoh_session: Arc<Session>,
+    foxglove_server: &FoxgloveWebSocket,
+    type_name: &str,
+    json_shcema: &str,
+    latched: bool,
+) -> anyhow::Result<()> {
+    info!(topic, "Starting json subscriber");
+    let zenoh_subscriber = zenoh_session.declare_subscriber(topic).res().await.unwrap();
+    let foxglove_channel = foxglove_server
+        .create_publisher(
+            topic,
+            JSON_ENCODING,
+            type_name,
+            json_shcema,
+            Some("jsonschema"),
+            latched,
+        )
+        .await?;
+
+    tokio::spawn({
+        let topic = topic.to_owned();
+        async move {
+            let mut message_counter = 0;
+            loop {
+                let sample = zenoh_subscriber.recv_async().await.unwrap();
+                message_counter += 1;
+                let now = SystemTime::now();
+                let time_nanos = system_time_to_nanos(&now);
+                let payload: Vec<u8> = sample.value.try_into().unwrap();
+                foxglove_channel.send(time_nanos, &payload).await.unwrap();
+
+                if message_counter % 20 == 0 {
+                    info!(
+                        topic,
+                        message_counter, "{} sent {} messages", topic, message_counter
+                    );
+                }
+            }
+        }
+    });
+    Ok(())
+}
+
+#[allow(dead_code)]
+const GENERIC_JSON_SCHEMA: &str = r#"
+{
+"title": "GenericJsonSchema",
+"description": "Generic JSON Schema",
+"type": "object",
+"properties": {}
+}
+"#;
+
+const IKEA_DIMMER_JSON_SCHEMA: &str = r#"
+{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+      "action": {
+        "type": "string"
+      },
+      "battery": {
+        "type": "integer"
+      },
+      "brightness": {
+        "type": "integer"
+      },
+      "linkquality": {
+        "type": "integer"
+      }
+    },
+    "required": [
+      "action",
+      "battery",
+      "brightness",
+      "linkquality"
+    ]
+}
+"#;
+
+const MOTION_SENSOR_JSON_SCHEMA: &str = r#"
+{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+      "battery": {
+        "type": "integer"
+      },
+      "battery_low": {
+        "type": "boolean"
+      },
+      "linkquality": {
+        "type": "integer"
+      },
+      "occupancy": {
+        "type": "boolean"
+      },
+      "tamper": {
+        "type": "boolean"
+      },
+      "voltage": {
+        "type": "integer"
+      }
+    },
+    "required": [
+      "battery",
+      "battery_low",
+      "linkquality",
+      "occupancy",
+      "tamper",
+      "voltage"
+    ]
+  }
+"#;
+
+const CONTACT_SENSOR_JSON_SCHEMA: &str = r#"
+{
+"$schema": "http://json-schema.org/draft-04/schema#",
+"type": "object",
+"properties": {
+    "battery": {
+    "type": "integer"
+    },
+    "battery_low": {
+    "type": "boolean"
+    },
+    "contact": {
+    "type": "boolean"
+    },
+    "linkquality": {
+    "type": "integer"
+    },
+    "tamper": {
+    "type": "boolean"
+    },
+    "voltage": {
+    "type": "integer"
+    }
+},
+"required": [
+    "battery",
+    "battery_low",
+    "contact",
+    "linkquality",
+    "tamper",
+    "voltage"
+]
+}
+"#;
+
+const CLIMATE_SENSOR_JSON_SCHEMA: &str = r#"
+{
+"$schema": "http://json-schema.org/draft-04/schema#",
+"type": "object",
+"properties": {
+    "battery": {
+    "type": "integer"
+    },
+    "humidity": {
+    "type": "number"
+    },
+    "linkquality": {
+    "type": "integer"
+    },
+    "temperature": {
+    "type": "number"
+    },
+    "voltage": {
+    "type": "integer"
+    }
+},
+"required": [
+    "battery",
+    "humidity",
+    "linkquality",
+    "temperature",
+    "voltage"
+]
+}
+"#;
